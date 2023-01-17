43a8ead004e01b2e40c7b670819fe0f4985099f5
Abdelghani <abdghaniallag@gmail.com>
2023-01-17 19:55:46 +0100

    adding DB with to models (Post, Tag)

--- base/views.py
+++ base/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse
+from django.http import HttpRequest, HttpResponse
+from .models import Post, Tag
+
 
 def home(request):
-    return render(request,'base/index.html')
+    return render(request, 'base/index.html')
+
+
 def posts(request):
-    return render(request,'base/posts.html')
+    posts = Post.objects.filter(active=True)
+    context = {"posts": posts}
+
+    return render(request, 'base/posts.html',  context)
+
+
 def post(request):
-    return render(request,'base/post.html')
+    return render(request, 'base/post.html')
+
+
 def profile(request):
-    return render(request,'base/profile.html')
+    return render(request, 'base/profile.html')
