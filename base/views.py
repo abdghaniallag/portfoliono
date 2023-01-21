1139b310e60a31a0ce67aa1a9630bb8c2f49775f
Abdelghani <abdghaniallag@gmail.com>
2023-01-21 20:37:42 +0100

    adding Paginator and refactoring posts page

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Post, Tag
 from .forms import PostForm
 from .filters import PostFilte
+from django.core.paginator import Paginator , EmptyPage, PageNotAnInteger
 
 def home(request):
     posts = Post.objects.filter(active=True, featured=True)[0:3]
@@ -15,6 +16,15 @@ def posts(request):
     posts = Post.objects.filter(active=True)
     postfilter = PostFilte(request.GET, queryset=posts)
     posts = postfilter.qs
+    page= request.GET.get('page')
+    paginator=Paginator(posts, 3)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {"posts": posts,"postfilter":postfilter}
 
     return render(request, 'base/posts.html',  context)
